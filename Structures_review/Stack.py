65501df33ccf8618cc55c26145fce3e60443de42
Fernando Dalai <ferdalaixd@gmail.com>
2023-07-04 10:29:53 -0600

    Structures

---
+++ Structures_review/Stack.py
@@ -0,0 +1,32 @@
+class Stack:
+    def __init__(self):
+        self.Top = None
+        self.Stack = []
+
+    def GetTop(self):
+        return self.Top
+
+    def Add(self, Element):
+        self.Top = Element
+        self.Stack.append(Element)
+
+    def Remove(self):
+        self.Stack.pop()
+        self.Top = self.Stack[-1]
+
+    def __str__(self):
+        return str(self.Stack)
+
+    def __len__(self):
+        return len(self.Stack)
+
+MyStack = Stack()
+
+MyStack.Add(1)
+MyStack.Add(2)
+MyStack.Add(3)
+print(MyStack)
+
+MyStack.Remove()
+print(MyStack)
+print(len(MyStack))
